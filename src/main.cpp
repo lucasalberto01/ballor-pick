ca568922d99bddaca8ba51150dfb88634d846d08
Lucas Alberto <lucas.alberto98@gmail.com>
2023-03-10 19:53:48 -0300

    init project

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <PS4Controller.h>
+#include <L298NX2.h>
+
+#define IN1_A 34
+#define IN2_A 35
+
+#define IN1_B 32
+#define IN2_B 33
+L298NX2 motors(IN1_A, IN2_A, IN1_B, IN2_B);
+void setup()
+{
+  Serial.begin(115200);
+  PS4.begin("1a:2b:3c:01:01:01");
+  Serial.println("Ready.");
+}
+
+void loop()
+{
+  // Below has all accessible outputs from the controller
+  if (PS4.isConnected())
+  {
+
+    int lStick = PS4.LStickY();
+    int rStick = PS4.RStickY();
+
+    if (lStick > 10)
+    {
+      motors.forward();
+    }
+    else if (lStick < -10)
+    {
+      motors.backward();
+    }
+    else if (rStick > 10)
+    {
+      motors.forward();
+    }
+    else if (rStick < -10)
+    {
+      motors.backward();
+    }
+    else
+    {
+      motors.stop();
+    }
+
+    if (PS4.Charging())
+      Serial.println("The controller is charging");
+    if (PS4.Audio())
+      Serial.println("The controller has headphones attached");
+    if (PS4.Mic())
+      Serial.println("The controller has a mic attached");
+
+    Serial.printf("Battery Level : %d\n", PS4.Battery());
+    // Remove it when you're not trying to see the output
+    delay(10);
+  }
+}
